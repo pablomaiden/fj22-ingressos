92d7b212e83ae14b9f248ffd16e256a7a037d256
Pablo Guimarães <pablomaiden@gmail.com>
2021-04-07 14:11:56 -0300

    Enviar

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.ingresso.dao.FilmeDao;
@@ -83,6 +84,7 @@ public class SessaoController {
     }
 	
 	@DeleteMapping("/admin/sessao/{id}")
+	@ResponseBody
 	@Transactional
 	public void delete(@PathVariable("id") Integer id){			
 		sessaoDao.delete(id);
