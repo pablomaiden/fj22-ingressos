b5a3ad5f8b6b14c6368c84dc8f8067f448375530
Pablo Guimarães <pablomaiden@gmail.com>
2021-04-08 11:00:13 -0300

    Enviando as validações

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -23,6 +23,7 @@ import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
 import br.com.caelum.ingresso.model.form.SessaoForm;
+import br.com.caelum.ingresso.model.validate.GerenciadorDeSessao;
 
 @Controller
 public class SessaoController {
@@ -64,10 +65,17 @@ public class SessaoController {
 		
 		if (result.hasErrors())
 		    return form(form.getSalaId(),form);
-
-		Sessao sessao = form.toSessao(salaDao, filmeDao);
-		sessaoDao.save(sessao);
-		return new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");
+		
+		Sessao sessao                   = form.toSessao(salaDao, filmeDao);
+		List<Sessao> sessoesDaSala      = sessaoDao.buscaSessoesDaSala(sessao.getSala());
+		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoesDaSala);
+		
+		if(gerenciador.cabe(sessao)) {
+			sessaoDao.save(sessao);
+			return new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");			
+		}
+		
+		return form(form.getSalaId(), form);
 	}
 	
 	@GetMapping("/admin/sala/{id}/sessoes")
